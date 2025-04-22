6dfa6c5684df253ae2158494cef1481b5558010a
CHxNOBODY <khaopan.nas@gmail.com>
2025-04-22 18:26:05 +0700

    update code

--- main.java
+++ main.java
@@ -1,13 +1,37 @@
+import java.util.Scanner;
+
 public class main {
+    public static void main(String[] args) {
 
-  public static void main(String[] args) {
+      int age;
+      Scanner input_age = new Scanner(System.in);
 
-    int age = 10;
-    double price = 10.99;
-    char grade = 'A';
+      System.out.print("Enter your age: ");
+      age = input_age.nextInt();
 
-    System.out.println("Your grade is " + grade);
-    System.out.println("$ " + price);
-    System.out.println("You are " + age + " years old");
-  }
+      System.out.println("You are " + age + " years old.");
+
+      int number = 0;
+      Scanner input_number = new Scanner(System.in);
+
+      System.out.print("Enter your number: ");
+      number = input_number.nextInt();
+
+      while(true){
+          if(number <= 0) {
+            System.out.println("Please try again.");
+            break;
+          } else if(number >= 10) {
+            System.out.println("Please try again.");
+            break;
+          } else {
+            System.out.println("Thank you for enter.");
+            break;
+          }
+      }
+      
+      for(int i = 0; i < 10; i++) {
+        System.out.print(i);
+      }
+    }
 }
