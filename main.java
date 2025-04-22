d6e1e61d4bb5f5297a610b924e3acd1de5113f13
CHxNOBODY <khaopan.nas@gmail.com>
2025-04-22 11:20:54 +0700

    first upload

---
+++ main.java
@@ -0,0 +1,13 @@
+public class main {
+
+  public static void main(String[] args) {
+
+    int age = 10;
+    double price = 10.99;
+    char grade = 'A';
+
+    System.out.println("Your grade is " + grade);
+    System.out.println("$ " + price);
+    System.out.println("You are " + age + " years old");
+  }
+}
